f71a67b2632a4949c84a8c327cb28ad416b5ad02
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-02-09 21:40:56 -0300

    Added Sidebar

---
+++ src/layouts/main/MainLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import MainSidebar from '@/layouts/main/sidebar/MainSidebar.vue';
+import "./styles.css"
+</script>
+
+<template>
+  <v-locale-provider>
+    <v-container fluid class="pa-0">
+      <v-app
+        theme="WhatsappLight"
+      >
+
+        <MainSidebar />
+        <!--<VerticalHeaderVue />-->
+
+        <v-main>
+          <div>
+            <RouterView />
+          </div>
+          <v-container fluid class="pt-0">
+            <div>
+            </div>
+          </v-container>
+        </v-main>
+
+      </v-app>
+    </v-container>
+  </v-locale-provider>
+</template>
