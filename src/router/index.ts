9316a78546f8a6738219402b6fa2385bcacda1f2
0ldev <rm556544@fiap.com.br>
2025-02-16 13:50:28 -0800

    feat: replace Firebase authentication with Supabase and update routing logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import PublicRoutes from './PublicRoutes';
 import { useUIStore } from '@/stores/ui';
-import { auth } from '@/auth/firebase';
+import { supabase } from '@/auth/supabase';
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,33 +15,23 @@ export const router = createRouter({
     PublicRoutes
   ]
 });
-
-/*// Assuming you have a type/interface for your authentication store
-interface AuthStore {
-  user: User | null;
-  returnUrl: string | null;
-  login(username: string, password: string): Promise<void>;
-  logout(): void;
-}*/
-
+~
 router.beforeEach(async (to, from, next) => {
-  // redirect to login page if not logged in and trying to access a restricted page
   const publicPages = ['/'];
+  const { data: { session } } = await supabase.auth.getSession();
 
   const isPublicPage = publicPages.includes(to.path);
   const authRequired = !isPublicPage && to.matched.some((record) => record.meta.requiresAuth);
 
   // User not logged in and trying to access a restricted page
-  if (authRequired && !auth.user) {
-    //auth.returnUrl = to.fullPath; // Save the intended page
-    //next('/home');
-    next();
-  } else if (auth.user && to.path === '/home') {
+  if (authRequired && !session) {
+    next('/home');
+  } else if (session && to.path === '/home') {
     // User logged in and trying to access the login page
     next({
       query: {
         ...to.query,
-        redirect: auth.returnUrl !== '/' ? to.fullPath : undefined
+        redirect: to.fullPath !== '/' ? to.fullPath : undefined
       }
     });
   } else {
